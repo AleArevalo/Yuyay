11373f095213f68517875a262bf0699f03813923
Alexander Arévalo <alexander_arevalo@icloud.com>
2024-07-10 00:36:04 -0400

    chore: Add Yuyay logo and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Construyendo</h1>
+      <Image src="/yuyay.svg" alt="Yuyay Logo" className='dark:invert' width={300} height={300} priority />
     </main>
   )
 }
