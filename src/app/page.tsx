9df2f6b924a439f7ea788d8c94d8633491bf6139
Alexander Arévalo <alexander_arevalo@icloud.com>
2024-07-11 13:01:56 -0400

    feat: Update dark mode in "start" button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,8 +26,8 @@ export default function Home() {
               </div>
             </div>
             <div className="mt-8">
-              <a href="#" className="px-4 py-2 bg-indigo-600 text-white rounded-full hover:bg-indigo-700">Ver resultado</a>
-              <a href="#" className="px-4 py-2 bg-gray-200 text-gray-800 rounded-full ml-4 hover:bg-gray-300">Empezar</a>
+              <a href="#" className="px-4 py-2 bg-indigo-600 text-white hover:bg-indigo-700 rounded-full">Ver resultado</a>
+              <a href="#" className="px-4 py-2 bg-black dark:bg-gray-200 text-white dark:text-gray-800 hover:bg-gray-800 dark:hover:bg-gray-300 rounded-full ml-4">Empezar</a>
             </div>
           </div>
           <UndrawThoughtProcess classList="hidden md:block" width={750} height={350} />
