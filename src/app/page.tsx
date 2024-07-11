f568e7780d1a547385aa0bd6361586adef1dc716
Alexander Arévalo <alexander_arevalo@icloud.com>
2024-07-11 00:25:50 -0400

    feat: Add search component and update homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import { VoiceAssistant, UndrawThoughtProcess, UndrawSoftware, UndrawProudSelf } from './ui/icons/icons'
+import { IconSearch } from './ui/icons/icons'
+import { VoiceAssistant, UndrawThoughtProcess, UndrawSoftware, UndrawProudSelf,  } from '@/assets/images'
 
 export default function Home() {
   return (
@@ -9,39 +9,48 @@ export default function Home() {
           <div>
             <h1 className="text-4xl font-bold text-gray-400">Welcome to Yuyay</h1>
             <p className="my-4 text-gray-100">
-              The modern way to organize your thoughts with AI-powered mind maps.
+              La forma más moderna de organizar tus pensamientos con mapas mentales segmentados con IA.
             </p>
-            <input
-              type="text"
-              placeholder="Buscar en la mente de Yuyay..."
-              className="bg-transparent border border-gray-500 placeholder-gray-500 text-white rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300 ease-in-out w-4/6"
-            />
+            <div className="flex justify-center ">
+              <div className="relative w-4/6">
+                <input
+                  type="text"
+                  placeholder="Buscar en la mente de Yuyay..."
+                  className="bg-transparent border border-gray-500 placeholder-gray-500 text-white rounded-full px-4 py-2 pr-10 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300 ease-in-out w-full"
+                  maxLength={30}
+                />
+                <span className="absolute inset-y-0 right-3 flex items-center pointer-events-none">
+                  <IconSearch width={24} height={24} classList="text-gray-500" />
+                </span>
+              </div>
+            </div>
             <div className="mt-8">
-              <a href="#" className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700">Ver resultado</a>
-              <a href="#" className="px-4 py-2 bg-gray-200 text-gray-800 rounded ml-4 hover:bg-gray-300">Empezar</a>
+              <a href="#" className="px-4 py-2 bg-indigo-600 text-white rounded-full hover:bg-indigo-700">Ver resultado</a>
+              <a href="#" className="px-4 py-2 bg-gray-200 text-gray-800 rounded-full ml-4 hover:bg-gray-300">Empezar</a>
             </div>
           </div>
-          <div>
-            <VoiceAssistant width={550} height={250} />
-          </div>
+          <UndrawThoughtProcess width={750} height={350} />
         </div>
       </main>
 
-      <section className="bg-white py-16">
+      <section className="bg-indigo-600 py-16">
         <div className="container mx-auto px-6">
           <h2 className="text-2xl font-bold text-gray-900 text-center">Características</h2>
           <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div className="bg-gray-100 p-8 rounded shadow-md text-center">
-              <h3 className="text-xl font-bold">Feature 1</h3>
-              <p className="mt-4 text-gray-600">Description of Feature 1</p>
+            <div className="bg-gray-100 p-8 rounded-3xl shadow-md text-center">
+              <h3 className="text-xl font-bold text-indigo-600">Transcripción de voz</h3>
+              <VoiceAssistant width={325} height={250} />
+              <p className="mt-4 text-gray-600">Habla como una nota de voz y la AI la guardará como una memoria</p>
             </div>
-            <div className="bg-gray-100 p-8 rounded shadow-md text-center">
-              <h3 className="text-xl font-bold">Feature 2</h3>
-              <p className="mt-4 text-gray-600">Description of Feature 2</p>
+            <div className="bg-gray-100 p-8 rounded-3xl shadow-md text-center">
+              <h3 className="text-xl font-bold text-indigo-600">Mapa mental inteligente</h3>
+              <UndrawSoftware width={325} height={250} />
+              <p className="mt-4 text-gray-600">Las memorias serán presentadas como mapas dinámicos e interactivos</p>
             </div>
-            <div className="bg-gray-100 p-8 rounded shadow-md text-center">
-              <h3 className="text-xl font-bold">Feature 3</h3>
-              <p className="mt-4 text-gray-600">Description of Feature 3</p>
+            <div className="bg-gray-100 p-8 rounded-3xl shadow-md text-center">
+              <h3 className="text-xl font-bold text-indigo-600">Memoria segmentada</h3>
+              <UndrawProudSelf width={325} height={250} />
+              <p className="mt-4 text-gray-600">Búsqueda rápida y precisa con resumen o como nota de voz</p>
             </div>
           </div>
         </div>
---
+++ src/app/ui/icons/icons.tsx
@@ -0,0 +1,18 @@
+export const IconSearch = ({ width = 500, height = 500, classList = '' }: { width?: number; height?: number; classList?: string; }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height={height}
+    className={classList}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+    <path d="M21 21l-6 -6" />
+  </svg>
+)
