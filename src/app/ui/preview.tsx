6edf3b71a053a139c3f34ff541da55bef8f7ecdc
Alexander Arévalo <alexander_arevalo@icloud.com>
2024-07-14 13:39:47 -0400

    feat: Update MindMap component to use responsive SVG size and improve node styling

--- src/app/ui/preview.tsx
+++ src/app/ui/preview.tsx
@@ -1,7 +1,6 @@
 import MindMap from './mindmap'
 
 const nodes = [
-    { id: 'Yuyay', info: 'Este es el nodo raíz.' },
     { id: 'Educativo', info: 'Este es el primer hijo.' },
     { id: 'Cinematografía', info: 'Este es el segundo hijo.' },
     { id: 'Memes', info: 'Este es el tercer hijo.' },
@@ -25,23 +24,17 @@ const nodes = [
 ]
 
 const links = [
-    { source: 'Yuyay', target: 'Memes', value: 1 },
-    { source: 'Yuyay', target: 'Educativo', value: 1 },
     { source: 'Educativo', target: 'Matemáticas', value: 1 },
     { source: 'Educativo', target: 'Literatura', value: 1 },
-    { source: 'Yuyay', target: 'Cinematografía', value: 1 },
     { source: 'Cinematografía', target: 'Películas', value: 1 },
     { source: 'Cinematografía', target: 'Series', value: 1 },
     { source: 'Cinematografía', target: 'Documentales', value: 1 },
-    { source: 'Yuyay', target: 'Recuerdos', value: 1 },
     { source: 'Recuerdos', target: 'Cumpleaños', value: 1 },
     { source: 'Cumpleaños', target: 'Oficina', value: 1 },
     { source: 'Oficina', target: 'Lista negra', value: 1 },
-    { source: 'Yuyay', target: 'Deportes', value: 1 },
     { source: 'Deportes', target: 'Fútbol', value: 1 },
     { source: 'Deportes', target: 'Baloncesto', value: 1 },
     { source: 'Deportes', target: 'Tenis', value: 1 },
-    { source: 'Yuyay', target: 'Ciencia', value: 1 },
     { source: 'Ciencia', target: 'Biología', value: 1 },
     { source: 'Ciencia', target: 'Química', value: 1 },
     { source: 'Ciencia', target: 'Física', value: 1 },
@@ -52,8 +45,8 @@ const Preview = () => {
     return (
         <article id="preview" className="py-16">
             <div className="container mx-auto px-6">
-                <h2 className="text-2xl text-white font-bold text-center">Vista previa</h2>
-                <section className="mt-8 bg-white">
+                <h2 className="text-2xl font-bold text-center">Vista previa</h2>
+                <section className="mt-8 bg-white dark:bg-[#17181c] rounded-full">
                     <MindMap nodes={nodes} links={links} />
                 </section>
             </div>
