858e821516b2efd2befd0d68e925392ed2b8108b
Alexander Arévalo <alexander_arevalo@icloud.com>
2024-07-15 16:24:57 -0400

    feat: Add interactive buttons for searching in simulation memory

--- src/app/ui/preview.tsx
+++ src/app/ui/preview.tsx
@@ -10,6 +10,20 @@ const Preview = () => {
                 <section className="flex mt-8 bg-white dark:bg-[#17181c] border-2 rounded-xl md:rounded-full">
                     <MindMap nodes={nodes} links={links} />
                 </section>
+                <div className="text-center py-4">
+                    Prueba buscando en la memoria de simulación para ver cómo funciona
+                </div>
+                <div className="grid md:grid-cols-3 gap-4 mx-8">
+                    <button className="px-4 py-2 bg-indigo-600 text-white hover:bg-indigo-700 rounded-full">
+                        ¿Cómo se multiplica dos números?
+                    </button>
+                    <button className="px-4 py-2 bg-indigo-600 text-white hover:bg-indigo-700 rounded-full">
+                        ¿Última serie que deje pendiente?
+                    </button>
+                    <button className="px-4 py-2 bg-indigo-600 text-white hover:bg-indigo-700 rounded-full">
+                        Mis notas del curso de nodeJS
+                    </button>
+                </div>
             </div>
         </article>
     )
